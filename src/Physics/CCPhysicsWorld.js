d2ed90672cf45fb691b29bb2d5b79257cab9b5a4
Young-Hwan Mun <yh.msw9@gmail.com>
2015-01-04 02:43:16 -0500

    PyramidStack added.

--- src/Physics/CCPhysicsWorld.js
+++ src/Physics/CCPhysicsWorld.js
@@ -137,6 +137,7 @@ cc.PhysicsWorld = cc.Class.extend
 		this._debugDraw.setVisible ( false );
 		this._scene.addChild ( this._debugDraw, 1 );	
 		
+		// Temporary Code
 		return true;
 		
 		this._info.getSpace ( ).setDefaultCollisionHandler 
--- src/PhysicsTest.js
+++ src/PhysicsTest.js
@@ -25,7 +25,8 @@
 
 msw.PhysicTest = 
 [
- 	{ create:function ( ) { return new msw.LogoSmash ( ); } }
+ 	{ create:function ( ) { return new msw.LogoSmash 	( ); } },
+ 	{ create:function ( ) { return new msw.PyramidStack ( ); } }
 ];
 
 msw.PhysicTestIndex = 0;
@@ -37,6 +38,9 @@ var BASE_TEST_MENUITEM_NEXT_TAG 	= 3;
 var BASE_TEST_MENU_TAG 				= 10;
 var BASE_TEST_TITLE_TAG 			= 11;
 var BASE_TEST_SUBTITLE_TAG 			= 12;
+
+var	STATIC_COLOR					= cc.color ( 255, 0, 0, 255 );
+var	DRAG_BODYS_TAG 					= 0x80;
 
 msw.PhysicTestScene = cc.SceneEx.extend 
 ({
@@ -69,28 +73,28 @@ msw.PhysicTestScene = cc.SceneEx.extend
 		item3.tag = BASE_TEST_MENUITEM_NEXT_TAG;
 
 		var 	menu = new cc.Menu ( item1, item2, item3, item4 );
-		
+
 		menu.x = 0;
 		menu.y = 0;
-		
+
 		var 	width = item2.width, height = item2.height;
 		item1.x = SCR_W2 - width * 2;
-		item1.y = height;
+		item1.y = height / 2;
 		item2.x = SCR_W2;
-		item2.y = height;
+		item2.y = height / 2;
 		item3.x = SCR_W2 + width * 2;
-		item3.y = height;
+		item3.y = height / 2;
 		item4.x = SCR_W - 80;
 		item4.y = SCR_H - 20;
-		
+
 		this.addChild ( menu, 102, BASE_TEST_MENU_TAG );
 	},
-	
+
 	getTitle:function ( )
 	{
 		return "PhysicsTest";
 	},
-	
+
 	getSubTitle:function ( )
 	{
 		return "";
@@ -100,25 +104,46 @@ msw.PhysicTestScene = cc.SceneEx.extend
 	{
 		msw.PhysicTestScene.runThisTest ( );
 	},
-	
+
 	onNextCallback:function ( sender ) 
 	{
 		msw.PhysicTestIndex++;
 		msw.PhysicTestIndex = msw.PhysicTestIndex % msw.PhysicTest.length;
 		msw.PhysicTestScene.runThisTest ( );
 	},
-	
+
 	onBackCallback:function ( sender )
 	{
 		msw.PhysicTestIndex = ( msw.PhysicTestIndex == 0 ? msw.PhysicTest.length : msw.PhysicTestIndex ) - 1;		
 		msw.PhysicTestScene.runThisTest ( );
 	},	
-	
+
 	toggleDebug:function ( sender )
 	{
-	    this._debugDraw = !this._debugDraw;
-	    this.getPhysicsWorld ( ).setDebugDrawMask ( this._debugDraw ? cc.PhysicsWorld.DEBUGDRAW_ALL : cc.PhysicsWorld.DEBUGDRAW_NONE );
+		this._debugDraw = !this._debugDraw;
+		this.getPhysicsWorld ( ).setDebugDrawMask ( this._debugDraw ? cc.PhysicsWorld.DEBUGDRAW_ALL : cc.PhysicsWorld.DEBUGDRAW_NONE );
 	},
+
+	addGrossiniAtPosition:function ( p, scale )
+	{
+		if ( scale === undefined )	scale = 1.0;
+
+		var 	posx, posy;
+
+		posx = cc.random0To1 ( ) * 200.0;
+		posy = cc.random0To1 ( ) * 200.0;
+
+		posx = parseInt ( posx % 4 ) * 85;
+		posy = parseInt ( posy % 3 ) * 121;
+
+		var 	sp = new cc.SpriteEx ( this._spriteTexture, cc.rect ( posx, posy, 85, 121 ) );
+		sp.setScale ( scale );
+		sp.setPhysicsBody ( cc.PhysicsBody.createBox ( cc.size ( 48.0 * scale, 108.0 * scale ) ) );
+		this.addChild ( sp );
+		sp.setPosition ( p );
+
+		return sp;
+	}	
 });
 
 msw.PhysicTestScene.runThisTest = function ( )
@@ -126,3 +151,161 @@ msw.PhysicTestScene.runThisTest = function ( )
 	var		scene = msw.PhysicTest [ msw.PhysicTestIndex ].create ( );
 	cc.director.runScene ( scene );	
 }
+
+/////////////////////////////////////////////
+var		logo_width 		= 188;
+var		logo_height 	= 35;
+var		logo_row_length = 24;
+var 	logo_image 		=
+[
+	 15,-16,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,7,-64,15,63,-32,-2,0,0,0,0,0,0,0,
+	 0,0,0,0,0,0,0,0,0,0,0,31,-64,15,127,-125,-1,-128,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	 0,0,0,127,-64,15,127,15,-1,-64,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,-1,-64,15,-2,
+	 31,-1,-64,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,-1,-64,0,-4,63,-1,-32,0,0,0,0,0,0,
+	 0,0,0,0,0,0,0,0,0,0,1,-1,-64,15,-8,127,-1,-32,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	 1,-1,-64,0,-8,-15,-1,-32,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,-31,-1,-64,15,-8,-32,
+	 -1,-32,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,7,-15,-1,-64,9,-15,-32,-1,-32,0,0,0,0,0,
+	 0,0,0,0,0,0,0,0,0,0,31,-15,-1,-64,0,-15,-32,-1,-32,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	 0,0,63,-7,-1,-64,9,-29,-32,127,-61,-16,63,15,-61,-1,-8,31,-16,15,-8,126,7,-31,
+	 -8,31,-65,-7,-1,-64,9,-29,-32,0,7,-8,127,-97,-25,-1,-2,63,-8,31,-4,-1,15,-13,
+	 -4,63,-1,-3,-1,-64,9,-29,-32,0,7,-8,127,-97,-25,-1,-2,63,-8,31,-4,-1,15,-13,
+	 -2,63,-1,-3,-1,-64,9,-29,-32,0,7,-8,127,-97,-25,-1,-1,63,-4,63,-4,-1,15,-13,
+	 -2,63,-33,-1,-1,-32,9,-25,-32,0,7,-8,127,-97,-25,-1,-1,63,-4,63,-4,-1,15,-13,
+	 -1,63,-33,-1,-1,-16,9,-25,-32,0,7,-8,127,-97,-25,-1,-1,63,-4,63,-4,-1,15,-13,
+	 -1,63,-49,-1,-1,-8,9,-57,-32,0,7,-8,127,-97,-25,-8,-1,63,-2,127,-4,-1,15,-13,
+	 -1,-65,-49,-1,-1,-4,9,-57,-32,0,7,-8,127,-97,-25,-8,-1,63,-2,127,-4,-1,15,-13,
+	 -1,-65,-57,-1,-1,-2,9,-57,-32,0,7,-8,127,-97,-25,-8,-1,63,-2,127,-4,-1,15,-13,
+	 -1,-1,-57,-1,-1,-1,9,-57,-32,0,7,-1,-1,-97,-25,-8,-1,63,-1,-1,-4,-1,15,-13,-1,
+	 -1,-61,-1,-1,-1,-119,-57,-32,0,7,-1,-1,-97,-25,-8,-1,63,-1,-1,-4,-1,15,-13,-1,
+	 -1,-61,-1,-1,-1,-55,-49,-32,0,7,-1,-1,-97,-25,-8,-1,63,-1,-1,-4,-1,15,-13,-1,
+	 -1,-63,-1,-1,-1,-23,-49,-32,127,-57,-1,-1,-97,-25,-1,-1,63,-1,-1,-4,-1,15,-13,
+	 -1,-1,-63,-1,-1,-1,-16,-49,-32,-1,-25,-1,-1,-97,-25,-1,-1,63,-33,-5,-4,-1,15,
+	 -13,-1,-1,-64,-1,-9,-1,-7,-49,-32,-1,-25,-8,127,-97,-25,-1,-1,63,-33,-5,-4,-1,
+	 15,-13,-1,-1,-64,-1,-13,-1,-32,-49,-32,-1,-25,-8,127,-97,-25,-1,-2,63,-49,-13,
+	 -4,-1,15,-13,-1,-1,-64,127,-7,-1,-119,-17,-15,-1,-25,-8,127,-97,-25,-1,-2,63,
+	 -49,-13,-4,-1,15,-13,-3,-1,-64,127,-8,-2,15,-17,-1,-1,-25,-8,127,-97,-25,-1,
+	 -8,63,-49,-13,-4,-1,15,-13,-3,-1,-64,63,-4,120,0,-17,-1,-1,-25,-8,127,-97,-25,
+	 -8,0,63,-57,-29,-4,-1,15,-13,-4,-1,-64,63,-4,0,15,-17,-1,-1,-25,-8,127,-97,
+	 -25,-8,0,63,-57,-29,-4,-1,-1,-13,-4,-1,-64,31,-2,0,0,103,-1,-1,-57,-8,127,-97,
+	 -25,-8,0,63,-57,-29,-4,-1,-1,-13,-4,127,-64,31,-2,0,15,103,-1,-1,-57,-8,127,
+	 -97,-25,-8,0,63,-61,-61,-4,127,-1,-29,-4,127,-64,15,-8,0,0,55,-1,-1,-121,-8,
+	 127,-97,-25,-8,0,63,-61,-61,-4,127,-1,-29,-4,63,-64,15,-32,0,0,23,-1,-2,3,-16,
+	 63,15,-61,-16,0,31,-127,-127,-8,31,-1,-127,-8,31,-128,7,-128,0,0
+];
+
+msw.frand = function ( )
+{
+	return parseInt ( Math.random ( ) * 0xffffff );
+};
+
+msw.LogoSmash = msw.PhysicTestScene.extend 
+({
+	ctor:function ( ) 
+	{
+		this._super ( );
+
+		this.getPhysicsWorld ( ).setGravity ( cp.v ( 0, 0 ) );
+//		this.getPhysicsWorld ( ).setUpdateRate ( 5.0 );
+
+		this._ball = new cc.SpriteBatchNode ( "res/ball.png", logo_image.length );
+		this.addChild ( this._ball );		
+
+		for ( var y = 0; y < logo_height; ++y )
+		{
+			for ( var x = 0; x < logo_width; ++x )
+			{
+				if ( this.get_pixel ( x, y ) )
+				{									
+					var 	x_jitter = 0.05 * cc.random0To1 ( );
+					var 	y_jitter = 0.05 * cc.random0To1 ( );
+
+					var 	ball = this.makeBall 
+					(
+							cp.v ( 2 * ( x - logo_width / 2 + x_jitter ) + SCR_W2, 2 * ( logo_height-y + y_jitter ) + SCR_H2 - logo_height / 2 ),
+							0.95, cc.PhysicsMaterial ( 0.01, 0.0, 0.0 ) 
+					);
+
+					ball.getPhysicsBody ( ).setMass ( 1.0 );
+					ball.getPhysicsBody ( ).setMoment ( cc.PHYSICS_INFINITY );
+
+//					this._ball.addChild ( ball );
+					this.addChild ( ball );
+				}
+			}
+		}
+
+		var 	bullet = this.makeBall ( cp.v ( 400, 0 ), 10, cc.PhysicsMaterial ( cc.PHYSICS_INFINITY, 0, 0 ) );
+		bullet.getPhysicsBody ( ).setVelocity ( cp.v ( 200, 0 ) );	   
+		bullet.setPosition ( cp.v ( -500, SCR_H2 ) );	    
+//		this._ball.addChild ( bullet );
+		this.addChild ( bullet );	 
+	},
+
+	getTitle:function ( )
+	{
+		return "Logo Smash";
+	},	
+
+	get_pixel:function ( x, y )
+	{
+		return ( logo_image [ ( x >> 3 ) + y * logo_row_length ] >> ( ~x & 0x7 ) ) & 1;
+	},
+
+	makeBall:function ( point, radius, material )
+	{
+		var 	ball = new cc.SpriteEx ( this._ball.texture );
+		var 	body = cc.PhysicsBody.createCircle ( radius, material );		
+
+		ball.setScale ( 0.13 * radius );
+		ball.setPhysicsBody ( body );				
+		ball.setPosition ( point );
+
+		return ball;
+	}	
+});
+
+/////////////////////////////////////////////
+msw.PyramidStack = msw.PhysicTestScene.extend 
+({
+	ctor:function ( ) 
+	{
+		this._super ( );
+		
+		var 	node = new cc.NodeEx ( );
+		node.setPhysicsBody ( cc.PhysicsBody.createEdgeSegment ( cp.v ( 0, 50 ), cp.v ( SCR_W, 50 ) ) );
+		this.addChild ( node );
+
+		var 	ball = new cc.SpriteEx ( "res/ball.png" );
+		ball.setScale ( 1 );
+		ball.setTag ( 100 );
+		ball.setPhysicsBody ( cc.PhysicsBody.createCircle ( 10 ) );
+		ball.getPhysicsBody ( ).setTag ( DRAG_BODYS_TAG );
+		ball.setPosition ( cp.v ( SCR_W2, 60 ) );
+		this.addChild ( ball, 0, 100 );
+
+		for ( var i = 0; i < 14; i++ )
+		{
+			for ( var j = 0; j <= i; j++ )
+			{
+				var 	sp = this.addGrossiniAtPosition (  cp.v ( SCR_W2 + ( i / 2 - j ) * 11, ( 14 - i ) * 23 + 100 ), 0.2 );
+				sp.getPhysicsBody ( ).setTag ( DRAG_BODYS_TAG );
+			}
+		}		
+		
+		this.scheduleOnce ( this.updateOnce, 3.0 );
+	},
+	
+	getTitle:function ( )
+	{
+		return "Pyramid Stack";
+	},		
+	
+	updateOnce:function ( )
+	{
+		var 	ball = this.getChildByTag ( 100 );
+
+		ball.setScale ( ball.getScale ( ) * 3 );
+		ball.setPhysicsBody ( cc.PhysicsBody.createCircle ( 30 ) );
+		ball.getPhysicsBody ( ).setTag ( DRAG_BODYS_TAG );
+	},
+});
